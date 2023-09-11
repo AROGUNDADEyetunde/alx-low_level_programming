2540ba65df536dc409843b79a14269b6df4210c8
AROGUNDADE <arogundadeyetunde1994@gmail.com>
2023-09-11 09:36:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
-#!/bin/bash
 #include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
